2a8c700a4151355f97a772f2a457a413215fceac
wyh <wyh771093498@163.com>
2019-05-12 15:14:38 +0800

    获取手机验证码及补充注册页面的手机验证码功能

--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -5,6 +5,7 @@ import re
 from django.db import DatabaseError
 from django.urls import reverse
 from django.contrib.auth import login
+from django_redis import get_redis_connection
 
 from users.models import User
 from meiduo_mall.utils.response_code import RETCODE
@@ -41,9 +42,10 @@ class RegisterView(View):
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
         mobile = request.POST.get('mobile')
+        sms_code_client = request.POST.get('sms_code')
         allow = request.POST.get('allow')
 
-        if not all([username, password, password2, mobile, allow]):
+        if not all([username, password, password2, mobile, allow, sms_code_client]):
             return http.HttpResponseForbidden('缺少必传参数')
         if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
             return http.HttpResponseForbidden('请输入5-20个字符的用户名')
@@ -53,6 +55,15 @@ class RegisterView(View):
             return http.HttpResponseForbidden('两次密码输入不一致')
         if not re.match(r'^1[3-9]\d{9}$', mobile):
             return http.HttpResponseForbidden('请输入正确的手机号')
+
+        redis_conn = get_redis_connection('verify_code')
+        sms_code_server = redis_conn.get('sms_%s' % mobile)
+        sms_code_server = sms_code_server.decode()
+        if sms_code_server is None:
+            return render(request, 'register.html', {'sms_code_errmsg': '短信验证码已失效!'})
+        if sms_code_client != sms_code_server:
+            return render(request, 'register', {'sms_code_errmsg': '输入短信验证码有误!'})
+
         if allow != 'on':
             return http.HttpResponseForbidden('请勾选用户协议')
 
